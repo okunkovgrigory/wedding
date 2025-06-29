5308e9dc57270b49a78a54981c7684e9414a58ed
Григорий Окуньков <grigoriiokunkov@MacBook-Pro-Grigorij.local>
2025-06-29 21:05:53 +0300

    change fonts Patriciana -> Montserrat.

--- js/rsvp.js
+++ js/rsvp.js
@@ -24,7 +24,11 @@ document.addEventListener('DOMContentLoaded', function () {
     regret.className = 'form-regret';
     regret.style.display = 'none';
     regret.innerHTML = `
-        <div class="icon">❤</div>
+        <div class="icon">
+        <svg viewBox="0 0 24 24" width="60" height="60">
+            <path fill="currentColor" d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
+        </svg>
+        </div>
         <h2>Спасибо, что сообщили!</h2>
         <p>Очень жаль, что вы не сможете быть с нами в этот день.</p>
         <p>Мы будем скучать и обязательно поделимся с вами фотографиями!</p>
